b818ebe5667b0f425b0f356b1afc54603092c0af
B.V. Avinash <78400699+BVAvinash@users.noreply.github.com>
2022-07-06 16:29:24 +0530

    Update bms_checker.cpp

--- bms_checker.cpp
+++ bms_checker.cpp
@@ -1,28 +1,44 @@
 #include "bms_checker.hpp"
-#include <algorithm>
 
-bool bms::BatteryHealth::batteryIsOk(std::vector<float> batteryParam)
+bool bms::BatteryHealth::checkRange(float actualValue, float minValue, float maxValue) 
 {
-	for (int index = 0; index < batteryParam.size(); index++)
+	return (actualValue < minValue || actualValue > maxValue);
+}
+
+bool bms::BatteryHealth::batteryTemperatureIsOk(float temperature) 
+{
+	bool result = true;
+	if (checkRange(temperature, temperature_min, temperature_max))
 	{
-		if (index == 0 && (batteryParam[index] < temperature_min || batteryParam[index] > temperature_max))
-		{
-			std::cout << "Temperature out of range!\n";
-			return false;
-		}
-
-		if (index == 1 && (batteryParam[index] < soc_min || batteryParam[index] > soc_max))
-		{
-			std::cout << "State of Charge out of range!\n";
-			return false;
-		}
-
-		if (index == 2 && (batteryParam[index] > chargeRate_max))
-		{
-			std::cout << "Charge Rate out of range!\n";
-			return false;
-		}
+		std::cout << "Temperature out of range!\n";
+		result = false;
 	}
+	return result;
+}
 
-	return true;
+bool bms::BatteryHealth::batterySocIsOk(float soc) 
+{
+	bool result = true;
+	if (checkRange(soc, soc_min, soc_max))
+	{
+		std::cout << "State of Charge out of range!\n";
+		result = false;
+	}
+	return result;
+}
+
+bool bms::BatteryHealth::batteryChargeRateIsOk(float chargeRate) 
+{
+	bool result = true;
+	if (checkRange(chargeRate, chargeRate_min, chargeRate_max))
+	{
+		std::cout << "Charge Rate out of range!\n";
+		result = false;
+	}
+	return result;
+}
+
+bool bms::BatteryHealth::batteryIsOk(float temperature, float soc, float chargeRate)
+{
+	return (batteryTemperatureIsOk(temperature) && batterySocIsOk(soc) && batteryChargeRateIsOk(chargeRate));
 }
