fa1f6d8ef0d8dca34e7e1765afa60d0945f54047
B.V. Avinash <78400699+BVAvinash@users.noreply.github.com>
2022-07-06 16:30:15 +0530

    Update bms_param.hpp

--- bms_param.hpp
+++ bms_param.hpp
@@ -7,6 +7,7 @@ namespace bms
 	static float temperature_max = 45;
 	static float soc_min = 20;
 	static float soc_max = 80;
+	static float chargeRate_min = 0;
 	static float chargeRate_max = 0.8;
 }
 
