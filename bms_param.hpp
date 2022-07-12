ca2e06bbdfcffb33b13cbe368a4ed704f31884f1
B.V. Avinash <78400699+BVAvinash@users.noreply.github.com>
2022-07-12 15:49:44 +0530

    Update bms_param.hpp

--- bms_param.hpp
+++ bms_param.hpp
@@ -1,6 +1,9 @@
 #ifndef BMS_PARAM
 #define BMS_PARAM
 
+#include <vector>
+#include <string>
+
 namespace bms 
 {
 	static float temperature_min = 0;
@@ -9,6 +12,9 @@ namespace bms
 	static float soc_max = 80;
 	static float chargeRate_min = 0;
 	static float chargeRate_max = 0.8;
+	static int languageOption = 1;		//0 - English, 1- German
+	static std::vector<std::vector<std::string>> messageFactor = { { "Temperature", "State of charge", "Charge Rate" }, {"Temperatur", "Ladezustand", "Ladestrom"} };
+	static std::vector<std::string> messageTrailing = { "out of range!", "außerhalb des Bereichs!" };
 }
 
 #endif // !BMS_PARAM
