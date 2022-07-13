7ada5ed4ed58f169346c07e027a3bb2b14e9e408
B.V. Avinash <78400699+BVAvinash@users.noreply.github.com>
2022-07-13 19:23:26 +0530

    Update bms_checker.hpp

--- bms_checker.hpp
+++ bms_checker.hpp
@@ -14,7 +14,8 @@ namespace bms
         bool batterySocIsOk(float soc);
         bool batteryChargeRateIsOk(float chargeRate);
         bool checkRange(float actualValue, float minValue, float maxValue);
-        void checkBreach(float actualValue, float minValue, float maxValue, BATTERY_PARAMETER parameter);
+        void checkBreachLow(float actualValue, float minValue, float maxValue, BATTERY_PARAMETER parameter);
+        void checkBreachHigh(float actualValue, float maxValue, BATTERY_PARAMETER parameter);
         void printWarnMessage(BATTERY_PARAMETER parameter, PARAMETER_BREACH_TYPE breachType);
         void printErrorMessage(BATTERY_PARAMETER messageKey);
 
