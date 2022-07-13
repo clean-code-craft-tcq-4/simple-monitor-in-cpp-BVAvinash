add2fa0623d80339a999af854856f4a52214c5a5
B.V. Avinash <78400699+BVAvinash@users.noreply.github.com>
2022-07-13 19:10:37 +0530

    Update bms_runnable.cpp

--- bms_runnable.cpp
+++ bms_runnable.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	bms::BatteryHealth batteryStatus;
 	assert(batteryStatus.batteryIsOk(25, 70, 0.7) == true);
+	assert(batteryStatus.batteryIsOk(43, 70, 0.7) == true);
 	assert(batteryStatus.batteryIsOk(50, 85, 0) == false);
     return 0;
 }
